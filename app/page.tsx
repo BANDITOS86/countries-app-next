d3567f9e8a5a4a794013f7f7eb0f68c2e543562e
Aliaksandr <djvalentinoff@gmail.com>
2024-01-09 13:43:23 +0300

    feat: added a detailed country page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
-type Country = {
+export type Country = {
   name: {
     common: string;
   };
@@ -11,9 +12,19 @@ type Country = {
   translations: {
     rus: {
       common: string;
-    }
-  }
-}
+      official: string;
+    };
+  };
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  languages: {
+    [key: string]: string;
+  };
+  borders?: string[];
+  cca3: string;
+};
 
 async function getCountries(): Promise<Country[]> {
   const response = await fetch("https://restcountries.com/v3.1/all");
@@ -26,17 +37,16 @@ export default async function Home() {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 m">
       {countries.map((country) => (
-        <article
-          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl"
-          key={country.name.common}
-        >
-          <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
-            <Image src={country.flags.svg} fill alt={country.flags.svg} />
-          </div>
-          <h2 className="font-bold text-xl text-center mt-1">
-            {country.translations.rus.common}
-          </h2>
-        </article>
+        <Link href={`/country/${country.name.common}`} key={country.name.common}>
+          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl">
+            <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+              <Image src={country.flags.svg} fill alt={country.flags.svg} />
+            </div>
+            <h2 className="font-bold text-xl text-center mt-1">
+              {country.translations.rus.common}
+            </h2>
+          </article>
+        </Link>
       ))}
     </section>
   );
